e3629db251e9225e1844237691b020eb2f30205d
unknown <panja.souradeep@gmail.com>
2021-03-23 11:55:06 +0530

    azure changes

--- CosmosDbMVC/Models/Item.cs
+++ CosmosDbMVC/Models/Item.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,16 @@ namespace CosmosDbMVC.Models
 {
     public class Item
     {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "isComplete")]
+        public bool Completed { get; set; }
     }
 }
